9c2d4326b4b88892bbd305fb109d3304eb3d1833
alcfdez <alcfdez@hotmail.com>
2023-01-22 21:21:57 +0100

    init exercise first iteration

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -1,5 +1,27 @@
 package com.factoriaf5.kata;
 
 public class Character {
-    
+    int initHealth = 1000;
+    int initLevel = 1;
+    boolean alive = true;
 }
+
+public Character(){
+    this.initHealth = initHealth;
+    this.initLevel = initLevel;
+    this.alive = alive;
+}
+
+public int getInitHealth() {
+    return initHealth;
+}
+
+public int getInitLevel() {
+    return initLevel;
+}
+
+public boolean isAlive() {
+    return alive;
+}
+    
+
--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterTest {
-    
     @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
+    public void character_have_1000_health_level_1_and_is_alive() {
+        Character myCharacter = new Character ();
+assertEquals(myCharacter.initHealth, 1000);
+assertEquals(myCharacter.initLevel, 1);     assertEquals(myCharacter.alive,true)
+        
     }
 
 }
